a5071981dbf78af9d9efbe9e29cb059b0af455e9
curus-m <eliminationatonce@gmail.com>
2020-06-24 23:18:23 +0900

    create , update, delete

--- goods-serverless/src/dakimakura.js
+++ goods-serverless/src/dakimakura.js
@@ -1,3 +1,4 @@
+const AWS = require('aws-sdk');
 const { Pool } = require('pg')
 const queries = require('./queries.js')
 const pool = new Pool()
@@ -11,8 +12,78 @@ const createResponse = (status, body) => ({
     "body": JSON.stringify(body)
  });
 
+ const checkData  = function(data) {
+    const errors = [];
+
+    if(!data.name) {
+        errors.push("Name Error");
+    }
+    if (!data.brand) {
+        errors.push("Brand Error");
+    }
+    if (isNaN(data.price)) {
+        errors.push("Name Error");
+    }
+    if (isNaN(data.material)) {
+        errors.push("Material Error");
+    }
+    if (!data.releasedate) {
+        errors.push("Date Error");
+    }
+
+    return errors.length > 0 ? false : true;
+}
+ const yyyymmddhhmmss = function(date) {
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1); // getMonth() is zero-based
+    var dd  = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    var hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    var min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    var ss = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    return "".concat(yyyy).concat(mm).concat(dd).concat(hh).concat(min).concat(ss);
+   };
+ 
 exports.create = (event, ctx, callback) => {
-    callback(null, createResponse(200, { message: 'create' }));
+    const data = event.body.data;
+    const isOk = checkData(data);
+
+    if(!isOk) {
+        callback(null, createResponse(404, { message: 'Invalid Data!' }));
+    }
+    console.log("Data OK!")
+    const file = event.body.file;
+
+    (async (data,file) => {
+        const client = await pool.connect()
+        const query = queries.addDakimakura
+        const date = new Date();
+        let fileName;
+        try {
+            if (file) { 
+                fileName = yyyymmddhhmmss(date);
+                console.log(`fileName : ${fileName}`);
+                const s3 = new AWS.S3({region: 'ap-northeast-1'});
+                const uploadParams = {Bucket: 'resources/dakimakura/', Key: 'fileName', Body: file};
+                s3.upload (uploadParams, function (err, data) {
+                    if (err) {
+                        console.log("Error", err);
+                        throw Error(err);
+                    } 
+                    if (data) {
+                        console.log("Upload Success", data.Location);
+                    }
+                });
+            } else { 
+                fileName = "noimage.jpg";
+            }
+            const param = [data.name, data.brand, data.price, data.releaseDate, data.material, fileName]
+            const res = await client.query(query,param)
+            console.log("Successfully added");
+            callback(null, createResponse(200, { message: 'OK' }))            
+        } finally {
+            client.release()
+        }
+    })(data,file).catch(err => console.log(err.stack));
 };
   
 exports.getList = (event, ctx, callback) =>  {
@@ -44,11 +115,36 @@ exports.getItem = (event, ctx, callback) => {
     })(no).catch(err => console.log(err.stack));
 };
   
-exports.update = (event, ctx, cb) => {
-    cb(null, createResponse(200, {message: "test"} ));
+exports.update = (event, ctx, callback) => {
+    const data = event.body.data;
+    (async (data) => {
+        const client = await pool.connect()
+        const query = queries.updateDakimakura
+        const param = [data.id, data.name, data.brand, data.price, data.material, data.releaseDate]
+        try {
+            const res = await client.query(query,param)
+            console.log("Update Complete")
+            callback(null, createResponse(200, { message: 'OK' }))            
+        } finally {
+            client.release()
+        }
+    })(data).catch(err => console.log(err.stack));
+
 };
   
-exports.delete = (event, ctx, cb) => {
-    cb(null, createResponse(200, { message: 'delete' }));
+exports.delete = (event, ctx, callback) => {
+    const no = event.body.id;
+    (async (no) => {
+        const client = await pool.connect()
+        const query = queries.deleteDakimakura
+        const param = [no]
+        try {
+            const res = await client.query(query,param)
+            console.log(res.rows[0])
+            callback(null, createResponse(200, { message: 'OK' }))            
+        } finally {
+            client.release()
+        }
+    })(no).catch(err => console.log(err.stack));
 };
 
--- goods-serverless/src/queries.js
+++ goods-serverless/src/queries.js
@@ -1,9 +1,15 @@
+const { update } = require("./dakimakura");
+
 module.exports = {
     getDakiList: "select id, name, brand, price, releaseDate, material, description, image from dakimakura",
+    // limit 10;
     getDakiItem: "select id, name, brand, price, releaseDate, material, description, image from dakimakura where id=$1",
     insertDaki: "",
-    insertTest: `insert into human (id, name, age) values(nextval('seqtest'), $1, $2)`
+    insertTest: `insert into human (id, name, age) values(nextval('seqtest'), $1, $2)`,
+    addDakimakura: `insert into dakimakura (id, name, brand, price, releaseDate, material, image)
+                    values(nextval('goodsseq'), $1, $2, $3, $4, $5, $6)`,
+    deleteDakimakura: "delete from dakimakura where id = $1",
+    updateDakimakura: `update dakimakura set name=$2, brand=$3, price=$4, material=$5,
+                      releaseDate=$6 where id=$1`
+            
   }
-  
-  // "insert into dakimakura (no, title, brand, price, releaseDate, material, image, description) "
-  // + "values(nextval('goodsseq'),:title, :brand, :price,:releaseDate, :material, :image, :description)"; 
